08bcf7127c3dd5764803325809ffe22016c92f92
Nero Gamer <you@example.com>
2024-03-04 21:45:09 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace proyecto
+{
+    class progra3{
+        public static void Main(string[] args)
+        {
+            int mEent = 12345;
+            int mDiv = 12345 % 10;
+         Console.Writeline(mEent);
+         Console.Writeline(mDiv);
+        }
+    }
+}
