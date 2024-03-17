350e51481d106f05c3e69a3dc34ba9d1d2d1ab0c
nerogamer848 <132412649+nerogamer848@users.noreply.github.com>
2024-03-16 21:38:55 -0400

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        static int ContarPalabrasConDosVocales(string oracion)
+{
+    // Dividir la oración en palabras utilizando los espacios como delimitadores
+    string[] palabras = oracion.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+    int contador = 0;
+    // Contar las palabras que tienen al menos dos vocales
+    foreach (string palabra in palabras)
+    {
+        int cantidadVocales = 0;
+        foreach (char letra in palabra)
+        {
+            // Verificar si la letra es una vocal
+            if ("aeiouAEIOU".IndexOf(letra) != -1)
+            {
+                cantidadVocales++;
+                if (cantidadVocales >= 2)
+                {
+                    contador++;
+                    break; // Romper el bucle interno si ya se encontraron al menos dos vocales
+                }
+            }
+        }
+    }
+    return contador;
+}
+}
+}
