3f6025ee9728a8e7eb0b1e53cffbc3681cbc9b67
nerogamer848 <132412649+nerogamer848@users.noreply.github.com>
2024-03-16 21:40:11 -0400

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+
+class Program
+{
+   static void Main(string[] args)
+ {
+     int numero = 3557867;
+     int nuevoNumero = VoltearNumero(numero);
+     Console.WriteLine("Número original: " + numero);
+     Console.WriteLine("Número rotado: " + nuevoNumero);
+
+
+
+
+
+ }
+
+
+
+
+
+
+ static int VoltearNumero(int numero)
+ {
+     // Convertimos el número a una cadena para manipular sus dígitos
+     string numeroString = numero.ToString();
+
+     // Invertimos la cadena de dígitos
+     char[] arrayDigitos = numeroString.ToCharArray();
+     Array.Reverse(arrayDigitos);
+     numeroString = new string(arrayDigitos);
+
+     // Convertimos la cadena resultante de nuevo a un número
+     int nuevoNumero = int.Parse(numeroString);
+     return nuevoNumero;
+ }
+}
